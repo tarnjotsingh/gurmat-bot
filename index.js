47222746262f4c8938656814445f113058b19325
tarnjotsingh <tarnjotsingh@msn.com>
2020-08-19 22:02:24 +0100

    Saving changes

--- index.js
+++ index.js
@@ -3,25 +3,20 @@ import dotenv from 'dotenv'
 
 const bot = new Client();
 dotenv.config();
-const token = process.env.TOKEN;
 
-bot.login(token);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`)
 });
 
 bot.on('message', msg => {
-    if (msg.content === '!ping') {
-        msg.reply('pong');
-        msg.channel.send('pong');
-    } else if (msg.content.startsWith('!kick')) {
-        if (msg.mentions.users.size) {
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`You wanted to kick: ${taggedUser.username}`)
-        } else {
-            msg.reply('Please tag a valid user!')
-        }
+    //Probably best to create a command handler of sorts
+    switch(msg.content) {
+        case "!ping": msg.reply('pong!'); break;
+        default:
+            if(msg.content.startsWith('!'))
+                msg.reply("No valid command provided.... :rage:");
     }
 });
 
+bot.login(process.env.TOKEN);
