f2c55935dadc0a383281bac06fef43b1c8f6a79b
tarnjotsingh <tarnjotsingh@msn.com>
2020-08-19 10:01:17 +0100

    Interim commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hey there!");
+import { Client } from 'discord.js';
+
+const bot = new Client();
+const token = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`)
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+    } else if (msg.content.startsWith('!kick')) {
+        if (msg.mentions.users.size) {
+            const taggedUser = msg.mentions.users.first();
+            msg.channel.send(`You wanted to kick: ${taggedUser.username}`)
+        } else {
+            msg.reply('Please tag a valid user!')
+        }
+    }
+});
+
