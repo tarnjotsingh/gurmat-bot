64409b30319b50c6a79c2c6257a9f805c21663f6
tarnjotsingh <tarnjotsingh@msn.com>
2020-08-19 11:03:57 +0100

    Basic discord bot implementatioN

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import { Client } from 'discord.js';
+import dotenv from 'dotenv'
 
 const bot = new Client();
+dotenv.config();
 const token = process.env.TOKEN;
 
-bot.login(TOKEN);
+bot.login(token);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`)
 });
 
 bot.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (msg.content === '!ping') {
         msg.reply('pong');
         msg.channel.send('pong');
     } else if (msg.content.startsWith('!kick')) {
