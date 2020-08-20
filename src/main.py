ed71a3ef88a81547e5d3864811cc7bec41516bcf
tarnjotsingh <tarnjotsingh@msn.com>
2020-08-20 19:48:35 +0100

    Adding the vaheguru responses
    - May need to add a cooldown for it though since users have been
      spamming it LOTS in the vaheguru channel.

--- src/main.py
+++ src/main.py
@@ -13,6 +13,17 @@ station = None
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+@bot.event
+async def on_message(msg):
+    author = msg.author
+    channel = msg.channel
+    vaheguru_list = ['ਵਾਹਿਗੁਰੂ', 'vaheguru', 'vaheguroo', 'waheguru', 'waheguroo']
+
+    if msg.content in vaheguru_list:
+        await msg.channel.send(f"{msg.author.mention} ਵਾਹਿਗੁਰੂ")
+
+    await bot.process_commands(msg)
+
 
 @bot.command(pass_context=True)
 async def ping(ctx):
@@ -28,7 +39,7 @@ async def join(ctx):
         await ctx.message.add_reaction('🙏🏼')
         await connected.channel.connect()
     else:
-        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, you need to join a voice channel first ji.")
+        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, you need to join a voice channel first ji")
 
 
 @bot.command(pass_context=True)
@@ -37,7 +48,7 @@ async def leave(ctx):
         await ctx.message.add_reaction('🙏🏼')
         await ctx.voice_client.disconnect()
     else:
-        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, I'm not in a voice channel.")
+        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, I'm not in a voice channel")
 
 
 @bot.command(pass_context=True, aliases=['p', 'pla'])
@@ -51,7 +62,7 @@ async def play(ctx, stream_alias: str = "247kirtan"):
 
     # Set the station object
     if station and station.stream_alias.__eq__(stream_alias):
-        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, the selected station is already playing ji.")
+        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, the selected station is already playing ji")
     else:
         station = Station(stream_alias)
         # Restart/Start the stream based on the configured station
@@ -69,7 +80,7 @@ async def stop(ctx):
         await ctx.message.add_reaction('🙏🏼')
         ctx.voice_client.stop()
     else:
-        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, there isn't anything playing ji!")
+        await ctx.channel.send(f"{ctx.author.mention} ਵਾਹਿਗੁਰੂ, there isn't anything playing ji")
 
 
 bot.run(TOKEN)
